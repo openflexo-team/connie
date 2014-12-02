082f9236596445c5d7a61d107cd27abb4006641b
Vincent <vincent@openflexo.org>
2014-12-02 22:21:24 +0100

    MEDIUM / fix potential deadlock with file chooser for windows

--- flexoutils/src/main/java/org/openflexo/toolbox/ToolBox.java
+++ flexoutils/src/main/java/org/openflexo/toolbox/ToolBox.java
@@ -47,12 +47,16 @@ import java.util.TreeMap;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
 import javax.swing.JOptionPane;
 import javax.swing.UIManager;
+import javax.swing.filechooser.FileView;
 
 import org.jdom2.Document;
 import org.jdom2.JDOMException;
 import org.jdom2.input.SAXBuilder;
+import org.openflexo.rm.ResourceLocator;
 
 /**
  * @author bmangez <B>Class Description</B>
@@ -69,6 +73,10 @@ public class ToolBox {
 
 	public static final String OTHER = "OTHER";
 
+
+	public static final ImageIcon FOLDER_ICON = new ImageIconResource(ResourceLocator.locateResource("Icons/Common/Folder.gif"));
+	
+	
 	static {
 		String osName = System.getProperty("os.name");
 		if (osName.indexOf("Mac OS") > -1) {
@@ -766,7 +774,44 @@ public class ToolBox {
 		return fileChooserRequiresFix;
 	}
 
+	// A deadlock can occurs with windows when retrieving a Folder Icon
+	// The following fixes that:
+	//  - first check a user defined icon,
+	//  - if no user defined icons, if it is a directory we return our own folder icon,
+	//		so that there is no possible deadlock
+	//  - Otherwise find the system defined icon.
+	public static Icon getIconFileChooserWithFix(File f, FileView view){
+		if(ToolBox.isWindows()){
+			if(view!=null && view.getIcon(f)!=null){
+				return view.getIcon(f);
+			}else if(f.isDirectory()){
+				return FOLDER_ICON;
+			}
+		}
+		return null;
+	}
+	
+	public static boolean getIconFileChooserRequiresFix(File f, FileView view){
+		if(ToolBox.isWindows()){
+			if(view!=null && view.getIcon(f)!=null){
+				return true;
+			}else if(f.isDirectory()){
+				return true;
+			}
+		}
+		return false;
+	}
+	
 	public static void fixFileChooser() {
+		String[] cmd = new String[] { "regsvr32", "/u", "/s", System.getenv("windir") + "\\system32\\zipfldr.dll" };
+		try {
+			Runtime.getRuntime().exec(cmd);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void fixFileChooserDeadlock() {
 		String[] cmd = new String[] { "regsvr32", "/u", "/s", System.getenv("windir") + "\\system32\\zipfldr.dll" };
 		try {
 			Runtime.getRuntime().exec(cmd);
