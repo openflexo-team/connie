8555ae5bbb227e9f1433988d6fbe75b52b3ddd74
Sylvain Guérin <sylvain@openflexo.org>
2015-02-09 11:06:04 +0100

    LOW / Javadoc

---
+++ connie-core/src/main/java/org/openflexo/connie/package-info.java
@@ -0,0 +1,16 @@
+/**
+ * Core package containing base API for bindings<br>
+ * 
+ * CONNIE is a Java library allowing to define and execute an expression-based language over a generic API.
+ *
+ * This language is generally used to connect heterogeneous technologies with the same API.
+ *
+ * The purpose of CONNIE is to offer the ability to:<ul>
+ * <li>Navigate through a graph of objects mixing heterogeneous technologies.</li>æî
+ * <li>Define strong-typed paths and expressions.</li>
+ * <li>Execute strong-typed paths and expressions.</li>
+ * </ul>
+ * 
+ * @author sylvain
+ */
+package org.openflexo.connie;
