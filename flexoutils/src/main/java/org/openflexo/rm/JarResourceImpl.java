91cd38d03c453c4e82b23a7272ce358b2d8001e4
Sylvain Guérin <sylvain@openflexo.org>
2016-09-15 18:39:19 +0200

    LOW / Removed log

--- flexoutils/src/main/java/org/openflexo/rm/JarResourceImpl.java
+++ flexoutils/src/main/java/org/openflexo/rm/JarResourceImpl.java
@@ -157,7 +157,7 @@ public class JarResourceImpl extends BasicResourceImpl implements Resource {
 
 	private void loadJarFile() throws MalformedURLException, LocatorNotFoundException {
 
-		System.out.println("Loading " + jarfile);
+		//System.out.println("Loading " + jarfile);
 
 		contents = new HashMap<JarEntry, InJarResourceImpl>();
 		URL url = getURL();
@@ -173,7 +173,7 @@ public class JarResourceImpl extends BasicResourceImpl implements Resource {
 				name = name.substring(0, name.length() - 1);
 			}
 			String shortName = name.substring(name.lastIndexOf("/") + 1);
-			System.out.println("> " + name + " [" + shortName + "]");
+			//System.out.println("> " + name + " [" + shortName + "]");
 			InJarResourceImpl inJarResource = new InJarResourceImpl(name,
 					new URL("jar", url.getHost(), "file:" + jarfilename + "!/" + name));
 			inJarResource.setEntry(current);
