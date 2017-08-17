9371158c7256faed001e0aa4c2437f750e252d74
Sylvain Guérin <sylvain@openflexo.org>
2017-08-17 15:49:53 +0200

    MEDIUM / Support for LevenshteinDistance between 2 strings

--- flexoutils/src/main/java/org/openflexo/toolbox/StringUtils.java
+++ flexoutils/src/main/java/org/openflexo/toolbox/StringUtils.java
@@ -601,11 +601,14 @@ public class StringUtils {
 
 	/**
 	 * Returns true if the name have one of the given extensions. It supports multiple extensions ('.foo.bar').
-	 * @param name the name to test
-	 * @param possibleExtensions the possible extensions in <b>lower case</b>.
+	 * 
+	 * @param name
+	 *            the name to test
+	 * @param possibleExtensions
+	 *            the possible extensions in <b>lower case</b>.
 	 * @return true if one extension is found, false otherwise.
 	 */
-	public static boolean hasExtension(String name, String ... possibleExtensions) {
+	public static boolean hasExtension(String name, String... possibleExtensions) {
 		name = name.toLowerCase();
 
 		int dotIndex = name.indexOf('.');
@@ -613,11 +616,60 @@ public class StringUtils {
 
 			String extension = name.substring(dotIndex);
 			for (String possibleExtension : possibleExtensions) {
-				if (possibleExtension.equals(extension)) return true;
+				if (possibleExtension.equals(extension))
+					return true;
 			}
-			dotIndex = name.indexOf('.', dotIndex+1);
+			dotIndex = name.indexOf('.', dotIndex + 1);
 		}
 		return false;
+	}
+
+	/**
+	 * Computes levebshtein distance between 2 strings<br>
+	 * Original code from https://en.wikibooks.org/wiki/Algorithm_Implementation/Strings/Levenshtein_distance<br>
+	 * See https://creativecommons.org/licenses/by-sa/3.0/
+	 */
+	public static int levenshteinDistance(CharSequence lhs, CharSequence rhs) {
+		int len0 = lhs.length() + 1;
+		int len1 = rhs.length() + 1;
+
+		// the array of distances
+		int[] cost = new int[len0];
+		int[] newcost = new int[len0];
+
+		// initial cost of skipping prefix in String s0
+		for (int i = 0; i < len0; i++)
+			cost[i] = i;
+
+		// dynamically computing the array of distances
+
+		// transformation cost for each letter in s1
+		for (int j = 1; j < len1; j++) {
+			// initial cost of skipping prefix in String s1
+			newcost[0] = j;
+
+			// transformation cost for each letter in s0
+			for (int i = 1; i < len0; i++) {
+				// matching current letters in both strings
+				int match = (lhs.charAt(i - 1) == rhs.charAt(j - 1)) ? 0 : 1;
+
+				// computing cost for each transformation
+				int cost_replace = cost[i - 1] + match;
+				int cost_insert = cost[i] + 1;
+				int cost_delete = newcost[i - 1] + 1;
+
+				// keep minimum cost
+				newcost[i] = Math.min(Math.min(cost_insert, cost_delete), cost_replace);
+			}
+
+			// swap cost/newcost arrays
+			int[] swap = cost;
+			cost = newcost;
+			newcost = swap;
+		}
+
+		// the distance is the cost for transforming all letters in both strings
+		return cost[len0 - 1];
 	}
 
 }
