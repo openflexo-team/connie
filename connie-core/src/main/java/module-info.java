092d0aed31997143e4de821d62f603cf33c9f7e3
Fabien Dagnat <fabien.dagnat@imt-atlantique.fr>
2019-07-23 16:58:57 +0200

    porting connie-core to JPMS

---
+++ connie-core/src/main/java/module-info.java
@@ -0,0 +1,9 @@
+module org.openflexo.connie {
+    exports org.openflexo.connie;
+    requires org.openflexo.connie_utils;
+    requires org.openflexo.connie_parser;
+    requires java.logging;
+    requires java.desktop;
+    requires com.google.common;
+    requires org.apache.commons.lang3;
+}
