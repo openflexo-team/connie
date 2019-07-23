bc222ee3ee555fe067357e5ff625e976e6ec2d45
Fabien Dagnat <fabien.dagnat@imt-atlantique.fr>
2019-07-23 16:54:59 +0200

    porting testutils to JPMS + moving parser entry point to connie-parser

---
+++ connie-parser/src/main/java/module-info.java
@@ -0,0 +1,6 @@
+module org.openflexo.connie_parser {
+    exports org.openflexo.connie.expr.parser.analysis;
+    exports org.openflexo.connie.expr.parser.lexer;
+    exports org.openflexo.connie.expr.parser.node;
+    exports org.openflexo.connie.parser;
+}
