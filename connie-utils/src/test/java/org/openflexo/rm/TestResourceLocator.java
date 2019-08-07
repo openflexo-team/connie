1ad313ec136bd80c0d7163b2bd0b521ee6f43290
Fabien Dagnat <fabien.dagnat@imt-atlantique.fr>
2019-08-07 10:29:25 +0200

    tests on resource locator in one test file

--- connie-utils/src/test/java/org/openflexo/rm/TestResourceLocator.java
+++ connie-utils/src/test/java/org/openflexo/rm/TestResourceLocator.java
@@ -41,13 +41,21 @@ package org.openflexo.rm;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertSame;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assumptions.assumeTrue;
 
 import java.io.File;
+import java.util.List;
+import java.util.regex.Pattern;
+import java.util.stream.Stream;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestMethodOrder;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.openflexo.toolbox.ToolBox;
 
 /**
  * Test (some) ResourceLocator features
@@ -56,11 +64,12 @@ import org.junit.jupiter.api.TestMethodOrder;
  *
  */
 @TestMethodOrder(OrderAnnotation.class)
-public class TestResourceLocator {
-
+@DisplayName("Test for the resource locator")
+class TestResourceLocator {
 	@Test
 	@Order(1)
-	public void testLocateResourceInClassPath() {
+	@DisplayName("the resource is in the classpath")
+	void testLocateResourceInClassPath() {
 		Resource r = ResourceLocator.locateResource("Config/logging.properties");
 		assertNotNull(r);
 		assertTrue(r instanceof FileResourceImpl);
@@ -69,7 +78,8 @@ public class TestResourceLocator {
 
 	@Test
 	@Order(2)
-	public void testCache() {
+	@DisplayName("the resource is in the cache")
+	void testCache() {
 		Resource r = ResourceLocator.locateResource("Config/logging.properties");
 		Resource r2 = ResourceLocator.locateResource("Config/logging.properties");
 		assertSame(r, r2);
@@ -77,7 +87,8 @@ public class TestResourceLocator {
 
 	@Test
 	@Order(3)
-	public void testLocateResourceInSourceCode() {
+	@DisplayName("the resource is in the source code")
+	void testLocateResourceInSourceCode() {
 		Resource r = ResourceLocator.locateResource("Config/logging.properties");
 		Resource r2 = ResourceLocator.locateSourceCodeResource(r);
 		Resource r3 = ResourceLocator.locateSourceCodeResource("Config/logging.properties");
@@ -94,4 +105,75 @@ public class TestResourceLocator {
 
 	}
 
+	@ParameterizedTest
+	@Order(4)
+	@DisplayName("the resource is a list")
+	@MethodSource("provideResourceLocator")
+	void testListResources(ResourceLocatorDelegate rl) throws Exception {
+		Resource rloc = ResourceLocator.locateResource("Config");
+		assertTrue(rloc != null);
+		// rloc is not null
+		List<? extends Resource> list = rloc.getContents(Pattern.compile(".*[.]properties"), false);
+		assertTrue(list.size() == 7);
+		for (Resource r : list) {
+			System.out.println(r.getURI());
+		}
+	}
+
+	@ParameterizedTest
+	@Order(4)
+	@MethodSource("provideResourceLocator")
+	void testListResources2(ResourceLocatorDelegate rl) throws Exception {
+		Resource rloc = ResourceLocator.locateResource("TestDiff");
+		assertTrue(rloc != null);
+		System.out.println(rloc.getURI());
+
+		// This test tend to fail in eclipse because of the way eclipse manage resources
+		// Here we have resources that are java files! My solution is to tweak the run
+		// configuration for tests adding explicitly the folder containing the java files
+		// first in the classpath
+		List<? extends Resource> list = rloc.getContents(false);
+		assertTrue(list.size() == 8);
+		for (Resource r : list) {
+			System.out.println(r.getURI());
+		}
+	}
+
+	@Test
+	void testListResources3() {
+		new ClasspathResourceLocatorImpl();
+		// This does not work after Java 9, the corresponding module does not export FrameClose.wav
+		assumeTrue(ToolBox.getJavaVersion() < 9);
+		Resource rloc = ResourceLocator.locateResource("javax/swing/plaf/metal/sounds/FrameClose.wav");
+		assertTrue(rloc != null);
+		// rloc is not null
+		Resource container = rloc.getContainer();
+		if (container != null) {
+			Pattern pat = Pattern.compile(".*[.]wav");
+			assertTrue(rloc.getContainer().getContents(pat, false).size() > 1);
+			for (Resource r : rloc.getContainer().getContents(pat, false)) {
+				System.out.println(r.getURI());
+			}
+		}
+	}
+
+	@ParameterizedTest
+	@Order(4)
+	@MethodSource("provideResourceLocator")
+	void testMetaInf(ResourceLocatorDelegate rl) throws Exception {
+		Resource rloc = ResourceLocator.locateResource("META-INF");
+		assertTrue(rloc != null);
+		assertTrue(rloc instanceof FileResourceImpl);
+		System.out.println("Found META-INF here: " + rloc.getURI());
+	}
+
+	private static String PATH_SEP = System.getProperty("file.separator");
+
+	static Stream<ResourceLocatorDelegate> provideResourceLocator() {
+		FileSystemResourceLocatorImpl rl = new FileSystemResourceLocatorImpl();
+		File workingDirectory = new File(System.getProperty("user.dir"));
+		rl.prependToDirectories(workingDirectory + PATH_SEP + "src/main/resources");
+		rl.prependToDirectories(workingDirectory + PATH_SEP + "src/test/resources");
+		return Stream.of(new ClasspathResourceLocatorImpl(), rl);
+	}
 }
